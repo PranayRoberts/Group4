bba70228357ac9a8564f48fbc791dfd72ce2ee29
Harsha B <harshab6999@gmail.com>
2022-04-07 23:35:47 +0530

    Flight-Ticket-Booking-System~

---
+++ Flight-Ticket-Booking-System/src/main/java/com/example/FlightTicketBookingSystem/FlightTicketBookingSystemApplication.java
@@ -0,0 +1,14 @@
+package com.example.FlightTicketBookingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class FlightTicketBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightTicketBookingSystemApplication.class, args);
+	}
+
+}
